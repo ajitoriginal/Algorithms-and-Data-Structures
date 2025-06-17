85db84bbb535eef83ef8a1e93e9dd8e152a20345
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-06-17 13:17:57 +0530

    big o notation

--- DSA_Codevolution/Revision/notes.js
+++ DSA_Codevolution/Revision/notes.js
@@ -14,4 +14,23 @@ Analysis of algorithm
     --Space Complexity: Amount of memory taken by algorithm to run as a function of input size
     --There is no one solution that works best every single time. It is good to know multiple ways
       to solve a problem.
+Asymptotic Notations
+    --Mathematical tools to represent time and space complexity
+        --Big-O Notation : Worst case notation
+        --Omega Notation : Best case notation
+        --Theta Notation : Average case notation
+    
+    --Big-O Notation
+        --describes the complexity of an algorithm using algebraic terms
+        --represents worst case complexity of an algorithm
+        --two important characteristics
+            --expressed in terms of the input
+            --focuses on bigger picture
+        --Calculation of complexity
+            --O(n) : Linear
+            --O(1) : Constant
+            --O(n^2) : Quadratic
+            --O(n^3) : Cubic
+            --O(logn) : Logarithmic
+
 */
