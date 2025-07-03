64ee81764df73cd02cfca1752be020d5a8845197
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-07-03 11:05:20 +0530

    revision

---
+++ DSA_Codevolution/Revision3/notes.js
@@ -0,0 +1,8 @@
+/*
+An algorithm is a set of well-defined instructions to solve a particular problem
+
+Characteristics
+    Well defined inputs and outputs
+    Each step should be clear
+    Language independent
+ */
