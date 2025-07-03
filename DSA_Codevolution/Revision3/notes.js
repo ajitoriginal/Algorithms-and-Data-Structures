707d53e86814df8db4ecca69078e84f5fcc74b7a
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-07-03 13:16:43 +0530

    revision

--- DSA_Codevolution/Revision3/notes.js
+++ DSA_Codevolution/Revision3/notes.js
@@ -5,4 +5,8 @@ Characteristics
     Well defined inputs and outputs
     Each step should be clear
     Language independent
+
+Recursion is a problem solving technique where the solution depends on solutions to
+smaller instances of the same problem. Recursion is when a function calls itself. Every
+recursive solution needs to have a base case - a condition to terminate the recursion.
  */
